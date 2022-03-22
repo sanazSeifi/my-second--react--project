92adef96e301aef3be40fe623771094bd8640927
Sanaz <snz5seifi@gmail.com>
2022-03-22 13:44:48 -0400

    adding props

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import "./App.css";
 function App() {
   const [value, setValue] = useState("");
   const [keyValue, setKeyValue] = useState(false);
+  const [inputVal, setInputVal] = useState("");
   return (
     <div className="Sanaz">
       <input onClick={(e) => setValue(e.target.value)} />
-      <p>{value}</p>???
+      <p>{value}</p>
 
       <div onClick={()=> setKeyValue(!keyValue)}>
         click kon
       </div>
-      {keyValue === true ? <MyMainComponent /> : undefined}
-      <p>{keyValue}</p> 
+      {keyValue === true ? <MyMainComponent buttonTitle= {value} 
+      onInputChange={(inputVal)=> setInputVal(inputVal)}/> : undefined};
+      <button>{inputVal}</button>
+    
+      <p>{inputVal}</p> 
     </div>
   );
 }
